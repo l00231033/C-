3f52fca773b489ac3b439b2d3845409c9e115827
lijialong <8275562@qq.com>
2018-11-08 15:38:51 +0800

    fix

--- test/test001.cpp
+++ test/test001.cpp
@@ -1,36 +1,31 @@
 #include <iostream>
-#include <vector>
-#include <iterator> 
-#include <algorithm>
-#include <numeric>
 using namespace std;
  
 class Base 
 {
 protected:
-	int i = {0};
-		
+    int i = {0};
 };
  
 class Derived: public Base   //public修改成protected也是一样
 {	
 public:
     void fun(Derived &d)
-	{
-		d.i = 3;		//只有在派生类中才可以通过派生类对象访问基类的protected成员。
-	}
+    {
+        d.i = 3;		//只有在派生类中才可以通过派生类对象访问基类的protected成员。
+    }
     void print()
     {
         printf("===[%d]===\n", i);
     }
 };
- 
+
 int main()
 {
-	Derived derived;
+    Derived derived;
     derived.print();
-//	derived.i = 3;		 //只有在派生类中才可以通过派生类对象访问基类的protected成员。
+    //derived.i = 3;		 //只有在派生类中才可以通过派生类对象访问基类的protected成员。
     derived.fun(derived);
     derived.print();
-	return 0;			
+    return 0;			
 }
